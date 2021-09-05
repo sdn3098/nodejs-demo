721cffaf9d3175a58ff75fdefd04f029b8766811
Sumit Debnath <sdnatheh@gmail.com>
2021-09-05 17:03:05 +0530

    updated

--- source/controllers/controller.js
+++ source/controllers/controller.js
@@ -1,22 +1,25 @@
 // ****************************************************************
+// set up GET API
 // Controller functions to get the requested data from the models, 
 // create an HTML page displaying the data, and return it to the 
 // user to view in the browser.
 // ****************************************************************
 import path from 'path';
 const __dirname = path.resolve();
+
 // show html page
 export const home = (req, res) => {
     //show this file when the "/" is requested
     res.sendFile(__dirname+"/source/pages/home.html");
 }
-// get and show today's date
+// get today's date
 export const getTodayDate = (req, res) => {
     var dateObj = new Date();
     var month = dateObj.getUTCMonth() + 1; //months from 1-12
     var day = dateObj.getUTCDate();
     var year = dateObj.getUTCFullYear();
     var newdate = day+ "/" + month + "/" + year;
+
     res.json({
         today: newdate
     });
--- source/routes/route.js
+++ source/routes/route.js
@@ -2,25 +2,37 @@
 // "Routes" to forward the supported requests (and any information 
 // encoded in request URLs) to the appropriate controller functions.
 // *******************************************************************
+
 // import my GET API from the controller functions
-import { home, getTodayDate, getMonthsName,getIPeople } from '../controller/controller.js';
+import { 
+    home,
+    getTodayDate,
+    getMonthsName,
+    getPeople 
+} from '../controllers/controller.js';
+
 // set up the routing
 const routes = (app) => {
     // home page
     app.route('/')
         .get(home)
+        
     // GET home page.
     app.route('/home')
         .get(home)
+    
     // Get today's date
     app.route('/today')
         .get(getTodayDate)
+
     // get list of month names
     app.route('/months')
         .get(getMonthsName)
-    // get list of People
+
+    // get list of ITOT Devs
     app.route('/people')
-        .get(getItotDevs)
+        .get(getPeople)
 }
+
 // export the route
 export default routes;
